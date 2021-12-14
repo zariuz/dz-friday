9c1651ae8e3ef212326ea0aec163e0c3a9191e30
AndreyTverdovskiy <tverdovskiyandrey1996@gmail.com>
2021-12-14 19:15:49 +0300

    Add initialState in profile-reducer.ts, create action creators for change nickname and email, use all it in Profile.tsx

--- src/store/profile-reducer.ts
+++ src/store/profile-reducer.ts
@@ -1,9 +1,56 @@
-export const initialState = {}
+type initialStateType = {
+    _id: string;
+    email: string;
+    name: string;
+    avatar?: string;
+    publicCardPacksCount: number; // количество колод
 
-export const profileReducer = (state: any = initialState, action: any) => {
+    // created: Date;
+    // updated: Date;
+    isAdmin: boolean;
+    verified: boolean; // подтвердил ли почту
+    rememberMe: boolean;
+
+    error?: string;
+}
+
+export const initialState: initialStateType = {
+    _id: '1',
+    email: 'someEmail@umail.com',
+    name: 'Andrey',
+    avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    publicCardPacksCount: 2,
+    isAdmin: false,
+    verified: false,
+    rememberMe: false
+}
+
+export const ChangeNameAC = (name: string) => (
+    {
+        type: 'CHANGE-NAME',
+        name
+    } as const
+)
+
+export const ChangeEmailAC = (email: string) => (
+    {
+        type: 'CHANGE-MAIL',
+        email
+    } as const
+)
+
+
+type ActionsType =
+    ReturnType<typeof ChangeNameAC>
+    | ReturnType<typeof ChangeEmailAC>
+
+export const profileReducer = (state: initialStateType = initialState, action: ActionsType) => {
     switch (action.type) {
-        case 'login':
-            return {...state}
+        case 'CHANGE-NAME':
+            return {...state, name: action.name}
+
+        case 'CHANGE-MAIL':
+            return {...state, email: action.email}
         default:
             return state
     }
