40aded2946dcfdb8328edb82ab63e252df773868
kkovalenkoo <yvkovalenko1987@gmail.com>
2021-12-15 17:29:23 +0300

    point 9

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,6 +1,6 @@
 import {ChangeEvent, useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {resetErrorText, setError, userRegistration} from '../../store/register-Reducer'
+import {resetErrorText, setError, setLoading, userRegistration} from '../../store/register-Reducer'
 import {Navigate} from 'react-router-dom'
 import {AppRootStateType} from '../../store/store'
 
@@ -8,8 +8,9 @@ export const Register = () => {
 
     const dispatch = useDispatch()
 
-    const error = useSelector<AppRootStateType, boolean | null>(state => state.register.isError)
+    const error = useSelector<AppRootStateType, boolean>(state => state.register.isError)
     const errorText = useSelector<AppRootStateType, string | undefined>(state => state.register.errorText)
+    const loading = useSelector<AppRootStateType, boolean>(state => state.register.isLoading)
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -20,22 +21,22 @@ export const Register = () => {
     const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
         setEmail(e.currentTarget.value)
         dispatch(resetErrorText(''))
+        dispatch(setLoading(false))
     }
     const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
         setPassword(e.currentTarget.value)
         dispatch(resetErrorText(''))
+        dispatch(setLoading(false))
     }
 
     return (
         <div>
             {errorText}
-            {!error && <Navigate replace to={'/login'}/>}
-            <br/>
-            <input type="text" placeholder="email" onChange={onChangeEmail}/>
-            <br/>
-            <input type="text" placeholder="password" onChange={onChangePassword}/>
-            <br/>
-            <button onClick={onClickRegistration}>Registration</button>
+            {!errorText && loading && 'Loading...'}
+            {!error && <Navigate replace to={'/login'}/>}<br/>
+            <input type="text" placeholder="email" onChange={onChangeEmail}/><br/>
+            <input type="text" placeholder="password" onChange={onChangePassword}/><br/>
+            <button onClick={onClickRegistration} disabled={loading}>Registration</button>
         </div>
     )
 }
